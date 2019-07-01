71e1316d7874002791c7573b283f61ddd51e172a
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-07-01 21:45:42 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -13,7 +13,10 @@ end
 
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-    if age <= 
+    if age <= 12
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    else 
+      puts 
   
 end
 
