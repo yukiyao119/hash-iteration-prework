d2607b4de54ec89e30e33c44e1fd070d4ff7f6c8
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-07-01 21:46:05 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -16,7 +16,8 @@ def age_appropriate_birthday(birthday_kids)
     if age <= 12
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
     else 
-      puts 
-  
+      puts "You are too old for this."
+    end 
+  end 
 end
 
